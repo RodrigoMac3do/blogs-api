7f472c72f6438541683080f2a9a0d41e96775774
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-11-17 19:39:30 -0300

    add: função de erro personalizado

--- src/services/validations/validationSchema.js
+++ src/services/validations/validationSchema.js
@@ -1,5 +1,5 @@
 const httpException = require('../../utils/http.exception');
-const { mapStatusCode } = require('./statusCode');
+const { mapStatusCode, mapMessage } = require('./statusCode');
 
 const validateSchema = (schema, data) => {
   const { error, value } = schema.validate(data);
@@ -7,7 +7,7 @@ const validateSchema = (schema, data) => {
   if (error) {
     throw httpException(
       mapStatusCode(error.message),
-      'Some required fields are missing',
+      mapMessage(error.message),
     );
   }
 
