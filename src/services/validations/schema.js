c358abc20f149a4bc81fcf061766ef7d01c1ee82
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-11-17 19:36:24 -0300

    feat: schema para user

--- src/services/validations/schema.js
+++ src/services/validations/schema.js
@@ -5,6 +5,14 @@ const loginSchema = Joi.object({
   password: Joi.string().required(),
 });
 
+const userSchema = Joi.object({
+  displayName: Joi.string().min(8).required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required(),
+  image: Joi.string().uri().required(),
+});
+
 module.exports = {
   loginSchema,
+  userSchema,
 };
