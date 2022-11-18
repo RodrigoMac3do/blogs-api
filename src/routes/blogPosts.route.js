ee35055bc03db39614338a531f44b2234794d6e3
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-11-18 18:57:07 -0300

    feat: rota de post para post

--- src/routes/blogPosts.route.js
+++ src/routes/blogPosts.route.js
@@ -5,6 +5,13 @@ const middleware = require('../middlewares');
 const router = express.Router();
 
 router.get('/', middleware.auth.validateToken, controller.blogPosts.findAll);
-router.get('/:id', middleware.auth.validateToken, controller.blogPosts.findById);
+
+router.get(
+  '/:id',
+  middleware.auth.validateToken,
+  controller.blogPosts.findById,
+);
+
+router.post('/', middleware.auth.validateToken, controller.blogPosts.create);
 
 module.exports = router;
