892d0886f764abd3192c3d574faba894932db5cc
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-11-18 15:18:44 -0300

    feat: rota para remover usuario

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -7,5 +7,6 @@ const router = express.Router();
 router.get('/', middleware.auth.validateToken, controller.user.findAll);
 router.get('/:id', middleware.auth.validateToken, controller.user.findById);
 router.post('/', controller.user.create);
+router.delete('/me', middleware.auth.validateToken, controller.user.remove);
 
 module.exports = router;
