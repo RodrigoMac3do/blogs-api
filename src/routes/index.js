8424f164cc0ceec565e7d74319e70eee575f4b27
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-11-18 09:30:41 -0300

    add: import e export de categories

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const auth = require('./auth.route');
 const user = require('./user.route');
+const categories = require('./categories.route');
 
 module.exports = {
   auth,
   user,
+  categories,
 };
