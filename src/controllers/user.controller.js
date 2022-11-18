03cf31a0aea16bd3776a41b5c0f3115e697e4628
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-11-18 01:12:23 -0300

    feat: controller de get para user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -12,6 +12,22 @@ const create = async (req, res) => {
   return res.status(201).json({ token });
 };
 
+const findAll = async (_req, res) => {
+  const users = await service.user.findAll();
+
+  return res.status(200).json(users);
+};
+
+const findById = async (req, res) => {
+  const id = Number(req.params.id);
+
+  const user = await service.user.findById(id);
+
+  return res.status(200).json(user);
+};
+
 module.exports = {
   create,
+  findAll,
+  findById,
 };
