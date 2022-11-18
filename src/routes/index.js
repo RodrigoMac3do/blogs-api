bec7252cbc81c4a5ed3e9758a93f7154d7dce3ea
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-11-18 10:11:02 -0300

    add: import e export de blogPosts

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const auth = require('./auth.route');
 const user = require('./user.route');
 const categories = require('./categories.route');
+const blogPosts = require('./blogPosts.route');
 
 module.exports = {
   auth,
   user,
   categories,
+  blogPosts,
 };
