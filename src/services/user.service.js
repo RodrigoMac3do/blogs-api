624b7165be3d9c3a7865a5cbd261c8d804f4c10c
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-11-18 01:12:39 -0300

    feat: service de get para users

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -22,6 +22,27 @@ const create = async (body) => {
   return token;
 };
 
+const findAll = async () => {
+  const users = await User.findAll({
+    attributes: { exclude: ['password'] },
+  });
+
+  return users;
+};
+
+const findById = async (id) => {
+  const [user] = await User.findAll({
+    where: { id },
+    attributes: { exclude: ['password'] },
+  });
+
+  if (!user) throw httpException(404, 'User does not exist');
+
+  return user;
+};
+
 module.exports = {
   create,
+  findAll,
+  findById,
 };
