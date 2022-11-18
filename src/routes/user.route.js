0f9718b4d1e1adbdb9fa63d62c502af59d178170
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-11-18 01:12:04 -0300

    feat: rotas de get para user

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const controller = require('../controllers');
+const middleware = require('../middlewares');
 
 const router = express.Router();
 
+router.get('/', middleware.auth.validateToken, controller.user.findAll);
+router.get('/:id', middleware.auth.validateToken, controller.user.findById);
 router.post('/', controller.user.create);
 
 module.exports = router;
