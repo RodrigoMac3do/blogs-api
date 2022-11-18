0b7c2bcde38826b257d54095835f80eb231cd112
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-11-17 22:57:42 -0300

    refactor: remove senha do token jwt

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -2,23 +2,6 @@ const { User } = require('../models');
 const httpException = require('../utils/http.exception');
 const { createToken } = require('../utils/jwt.util');
 
-const findUser = async (body) => {
-  const { email, password } = body;
-
-  const user = await User.findOne({
-    where: {
-      password,
-      email,
-    },
-  });
-
-  if (!user) throw httpException(400, 'Invalid fields');
-
-  const token = createToken(user);
-
-  return token;
-};
-
 const create = async (body) => {
   const { displayName, email, password, image } = body;
 
@@ -32,12 +15,13 @@ const create = async (body) => {
 
   const newUser = await User.create({ displayName, email, password, image });
 
-  const token = createToken(newUser);
+  const { password: _, ...userWithoutPassword } = newUser.dataValues;
+
+  const token = createToken(userWithoutPassword);
 
   return token;
 };
 
 module.exports = {
-  findUser,
   create,
 };
