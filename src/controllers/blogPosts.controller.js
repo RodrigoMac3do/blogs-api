ff6dca1f98a576f84d09f0989651866450c0a58e
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-11-18 19:03:46 -0300

    feat: controller para criar um post

--- src/controllers/blogPosts.controller.js
+++ src/controllers/blogPosts.controller.js
@@ -1,4 +1,7 @@
 const service = require('../services');
+const { postSchema } = require('../services/validations/schema');
+const validateSchema = require('../services/validations/validationSchema');
+const { decode } = require('../utils/jwt.util');
 
 const findAll = async (_req, res) => {
   const posts = await service.blogPosts.findAll();
@@ -14,7 +17,24 @@ const findById = async (req, res) => {
   return res.status(200).json(post);
 };
 
+const create = async (req, res) => {
+  const { body } = req;
+  const { categoryIds } = body;
+  const { authorization } = req.headers;
+
+  const { id } = await decode(authorization);
+  
+  await validateSchema(postSchema, body);
+
+  await service.categories.findById(categoryIds);
+
+  const newPost = await service.blogPosts.create({ userId: id, ...body });
+
+  return res.status(201).json(newPost);
+};
+
 module.exports = {
   findAll,
   findById,
+  create,
 };
