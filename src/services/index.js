3d2904017eb35cf4a83ecf342a0a7e2758c5ce4b
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-11-18 19:27:49 -0300

    add: import e export de postCategories

--- src/services/index.js
+++ src/services/index.js
@@ -2,10 +2,12 @@ const user = require('./user.service');
 const auth = require('./auth.service');
 const categories = require('./categories.service');
 const blogPosts = require('./blogPosts.service');
+const postCategories = require('./postCategories.service');
 
 module.exports = {
   user,
   auth,
   categories,
   blogPosts,
+  postCategories,
 };
