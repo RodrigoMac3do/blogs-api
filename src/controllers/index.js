c11613f6ee106a5798b5571ab10a84e39b068315
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-11-18 09:31:26 -0300

    add: import e export de categories

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,7 +1,9 @@
 const auth = require('./auth.controller');
 const user = require('./user.controller');
+const categories = require('./categories.controller');
 
 module.exports = {
   auth,
   user,
+  categories,
 };
