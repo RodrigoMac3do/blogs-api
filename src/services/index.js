17b24c7fcadf2061ab6b8c8c32277ca38d57b7ee
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-11-18 10:12:34 -0300

    add: import e export de blogPosts

--- src/services/index.js
+++ src/services/index.js
@@ -1,9 +1,11 @@
 const user = require('./user.service');
 const auth = require('./auth.service');
 const categories = require('./categories.service');
+const blogPosts = require('./blogPosts.service');
 
 module.exports = {
   user,
   auth,
   categories,
+  blogPosts,
 };
