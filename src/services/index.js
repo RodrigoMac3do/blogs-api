1c7532a07ceece0522599543d7f10f66468263bd
Rodrigo Macedo <rodrigomac3do@gmail.com>
2022-11-18 09:33:33 -0300

    add: import e export de categories

--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,9 @@
 const user = require('./user.service');
 const auth = require('./auth.service');
+const categories = require('./categories.service');
 
 module.exports = {
   user,
   auth,
+  categories,
 };
