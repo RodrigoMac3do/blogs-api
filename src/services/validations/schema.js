d68b550d239b403e2da744ad6da18ee168ea446d
Rodrigo Macedo <rodrigomac3do@gmail.com>
2023-02-24 19:17:02 -0300

    refactor: loginSchema

--- src/services/validations/schema.js
+++ src/services/validations/schema.js
@@ -1,8 +1,12 @@
 const Joi = require('joi');
 
 const loginSchema = Joi.object({
-  email: Joi.string().email().required(),
-  password: Joi.string().required(),
+  email: Joi.string().email().required().messages({
+    'string.empty': 'Some required fields are missing',
+  }),
+  password: Joi.string().required().messages({
+    'string.empty': 'Some required fields are missing',
+  }),
 });
 
 const userSchema = Joi.object({
