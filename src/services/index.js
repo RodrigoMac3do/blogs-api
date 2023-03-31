2f79241f01ca8b57659349a32f180fb128478650
Rodrigo Macedo <rodrigomac3do@gmail.com>
2023-03-31 19:55:27 -0300

    fix: exclui importação de postCategories

--- src/services/index.js
+++ src/services/index.js
@@ -2,12 +2,10 @@ const login = require('./login.service');
 const user = require('./user.service');
 const categories = require('./categories.service');
 const blogPosts = require('./blogPosts.service');
-const postCategories = require('./postCategories.service');
 
 module.exports = {
   user,
   login,
   categories,
   blogPosts,
-  postCategories,
 };
